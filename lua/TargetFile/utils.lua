aca00ec023bf94ed5d7fb42e9fb4fdb35cea334f
zero_guru <38360323+zerogtiger@users.noreply.github.com>
2024-08-27 22:50:49 -0400

    Functions isolated

---
+++ lua/TargetFile/utils.lua
@@ -0,0 +1,23 @@
+local U = {}
+
+---@class Config
+---@field window_size number: how large the window is (only application to non-float options)
+---@field window_location number: Where the window appears (1: north; 2: east; 3: south; 4: west; 5: float)
+---@field supported_languages table: file extensions and the corresponding compile/execute commands
+
+U.defaults = {
+  window_size = 60,
+  window_location = 2,
+  supported_language = {},
+  leader = [[<leader>\]],
+}
+
+function U.file_ext(flnm)
+  return flnm:match("(%.%w+)$") or ""
+end
+
+function U.file_name(flnm)
+  return flnm:sub(string.find(flnm, "/[^/]*$") + 1, #flnm - #file_ext(flnm))
+end
+
+return U
